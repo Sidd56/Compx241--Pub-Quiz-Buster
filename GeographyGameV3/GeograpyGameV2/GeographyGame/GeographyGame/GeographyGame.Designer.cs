4b5d76ca31676ac9495a691ff71027c0ab64195a
Olegario <ro68@waikato.ac.nz>
2021-05-24 14:26:10 +1200

    geofiles

---
+++ GeographyGameV3/GeograpyGameV2/GeographyGame/GeographyGame/GeographyGame.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GeographyGame
+{
+    partial class GeographyGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.labelCountryName = new System.Windows.Forms.Label();
+            this.trackBarCountryStat = new System.Windows.Forms.TrackBar();
+            this.labelStat = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCountryStat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(172, 37);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(728, 429);
+            this.pictureBoxDisplay.TabIndex = 0;
+            this.pictureBoxDisplay.TabStop = false;
+            this.pictureBoxDisplay.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBoxDisplay.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBoxDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBoxDisplay.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // labelCountryName
+            // 
+            this.labelCountryName.AutoSize = true;
+            this.labelCountryName.Location = new System.Drawing.Point(12, 37);
+            this.labelCountryName.Name = "labelCountryName";
+            this.labelCountryName.Size = new System.Drawing.Size(46, 13);
+            this.labelCountryName.TabIndex = 1;
+            this.labelCountryName.Text = "Country:";
+            // 
+            // trackBarCountryStat
+            // 
+            this.trackBarCountryStat.Location = new System.Drawing.Point(12, 83);
+            this.trackBarCountryStat.Maximum = 4;
+            this.trackBarCountryStat.Name = "trackBarCountryStat";
+            this.trackBarCountryStat.Size = new System.Drawing.Size(139, 45);
+            this.trackBarCountryStat.TabIndex = 2;
+            this.trackBarCountryStat.TabStop = false;
+            this.trackBarCountryStat.Value = 4;
+            this.trackBarCountryStat.Scroll += new System.EventHandler(this.trackBarCountryStat_Scroll);
+            // 
+            // labelStat
+            // 
+            this.labelStat.AutoSize = true;
+            this.labelStat.Location = new System.Drawing.Point(12, 67);
+            this.labelStat.Name = "labelStat";
+            this.labelStat.Size = new System.Drawing.Size(35, 13);
+            this.labelStat.TabIndex = 3;
+            this.labelStat.Text = "label1";
+            // 
+            // GeographyGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 545);
+            this.Controls.Add(this.labelStat);
+            this.Controls.Add(this.trackBarCountryStat);
+            this.Controls.Add(this.labelCountryName);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Name = "GeographyGame";
+            this.Text = "GeographyGame";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarCountryStat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.Label labelCountryName;
+        private System.Windows.Forms.TrackBar trackBarCountryStat;
+        private System.Windows.Forms.Label labelStat;
+    }
+}
---
+++ GeographyGameV3/GeograpyGameV2/GeographyGame/GeographyGame/GeographyMenu.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace GeographyGame
+{
+    partial class GeographyMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.comboBoxContinent = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(29, 141);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(56, 27);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // comboBoxContinent
+            // 
+            this.comboBoxContinent.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxContinent.FormattingEnabled = true;
+            this.comboBoxContinent.Items.AddRange(new object[] {
+            "South America",
+            "Europe"});
+            this.comboBoxContinent.Location = new System.Drawing.Point(29, 114);
+            this.comboBoxContinent.Name = "comboBoxContinent";
+            this.comboBoxContinent.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxContinent.TabIndex = 1;
+            // 
+            // GeographyMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 256);
+            this.Controls.Add(this.comboBoxContinent);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "GeographyMenu";
+            this.Text = "Geography Menu";
+            this.Load += new System.EventHandler(this.GeographyMenu_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ComboBox comboBoxContinent;
+    }
+}
+
